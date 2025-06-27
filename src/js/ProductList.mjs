e6b1627842d8b6368efe70fb0b9fd0ecfb912246
Ricardo Salazar <therlokx@gmail.com>
2025-06-27 23:20:10 +0200

    w02 individual

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -17,17 +17,17 @@ export default class ProductList{
 }
 
 function productCardTemplate(product){
-    return `
-    <li class="product-card">
-            <a href="product_pages/?product=${product.Id}">
-              <img
-                src="${product.Image}"
-                alt="${product.NameWithoutBrand}"
-              />
-              <h3 class="card__brand">${product.Brand.Name}</h3>
-              <h2 class="card__name">${product.NameWithoutBrand}</h2>
-              <p class="product-card__price">$${product.FinalPrice}</p>
-            </a>
-          </li>
-    `
+  return `
+  <li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+    <img
+    src="${product.Image}"
+    alt="${product.NameWithoutBrand}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+  </li>
+  `  
 }
--- src/js/product.js
+++ src/js/product.js
@@ -4,6 +4,5 @@ import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
-
 const product = new ProductDetails(productId, dataSource);
 product.init();
